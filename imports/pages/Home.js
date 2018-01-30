2c5168186aaaaaeb01254346ecaef3239eb6798a
KingsleyChung <kingsleychung@outlook.com>
2018-01-30 11:48:48 +0800

    update file structure

---
+++ imports/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <NavigationBar />
+            </div>
+        )
+    }
+}
