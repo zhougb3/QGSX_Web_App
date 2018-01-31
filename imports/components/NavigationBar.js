c3c4b29fb4e84d7662d39977f035f7e0d1791b07
KingsleyChung <kingsleychung@outlook.com>
2018-01-31 09:12:24 +0800

    add material-ui

--- imports/components/NavigationBar.js
+++ imports/components/NavigationBar.js
@@ -5,10 +5,10 @@ import { Router, Route, Link } from 'react-router'
 export default class NavigationBar extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect>
+            <Navbar collapseOnSelect fixedTop={true}>
                 <Navbar.Header>
                     <Navbar.Brand>
-                    <a href="#brand">勤工善学</a>
+                    <a href="#brand" style={{color: "rgb()"}}>勤工善学</a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
@@ -23,7 +23,7 @@ export default class NavigationBar extends Component {
                         </NavItem>
                     </Nav>
                     <Nav pullRight>
-                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown eventKey={3} title={<img src="images/image.png" width={30}/>} id="basic-nav-dropdown">
                             <MenuItem eventKey={3.1}>Action</MenuItem>
                             <MenuItem eventKey={3.2}>Another action</MenuItem>
                             <MenuItem eventKey={3.3}>Something else here</MenuItem>
